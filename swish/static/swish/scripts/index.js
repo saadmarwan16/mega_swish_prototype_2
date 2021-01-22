8991e0bbba7ab5823d45b13401d1faf654e088f3
Marwan Sa-ad <saadmarwan16@gmail.com>
2021-01-22 19:26:03 +0300

    Initial commit

--- swish/static/swish/scripts/index.js
+++ swish/static/swish/scripts/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>
-        <h1>This is the real JSX!</h1>
-    </div>,
+    <React.StrictMode>
+        <div>
+            <h1>This is the real JSX!</h1>
+        </div>
+    </React.StrictMode>,
     document.getElementById('jsx')
-)
+)
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <h1>This is another real real JSX!</h1>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('another-jsx')
+)
